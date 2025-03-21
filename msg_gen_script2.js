fbee59e4511e326f388353ca3a47841fac959f07
Steven-codes-79 <152632394+Steven-codes-79@users.noreply.github.com>
2025-03-21 15:53:44 +0000

    add refined script2 to replace script1

---
+++ msg_gen_script2.js
@@ -0,0 +1,19 @@
+// MSG GENERATOR SCRIPT
+
+const JokeBase = () => {
+    const JokeBases = ['The past, present, and future', 'Comic Sans, Helvetica, and Times New Roman', 'An amnesiac', 
+        'Two dragons'];
+    return JokeBases[Math.floor(Math.random() * 4)] 
+  }; let result = JokeBase();
+  console.log(result);
+
+  const JokeAnchor = "walk(s) into a bar";
+  console.log(JokeAnchor);
+
+  const punchline = () => {
+    const line = ['It was tense.', 'Get out! shouts the barman. We don’t serve your type here!', 
+        'He goes up to a beautiful blonde and says, So, do I come here often?', 
+        'The first one says, It sure is hot in here. His friend snaps back, Shut your mouth!'];
+        return line [Math.floor(Math.random() * 4)]
+  }; let punchResult = punchline();
+  console.log(punchResult);
